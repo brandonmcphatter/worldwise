619d75cbf253406696216054f130ae9760d1800d
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-18 15:40:38 -0600

    'page not found'

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="product" element={<Product/>}/>
                 <Route path="pricing" element={<Pricing/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
