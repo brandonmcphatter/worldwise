e916f334cfb50f15af707a8fcd936acc0d8c3a7a
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-18 15:41:00 -0600

    links added . may change to nav comp

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,13 @@
+import {Link} from "react-router-dom";
 
 function Homepage() {
     return (
-        <div>Home</div>
+        <div>
+            <h1>Home</h1>
+            <Link to="/product">Product</Link>
+            <br/>
+            <Link to="/pricing">Pricing</Link>
+        </div>
     );
 }
 
