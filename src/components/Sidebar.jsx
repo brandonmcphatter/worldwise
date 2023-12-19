d669ba4cb42f9b395f3538c4205288fcf3605730
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-19 12:57:25 -0600

    created components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import styles from './Sidebar.module.css';
+import Logo from "./Logo.jsx";
+import AppNav from "./AppNav.jsx";
+import Footer from "./Footer.jsx";
+
+function Sidebar() {
+    return (
+        <div className={styles.sidebar}>
+            <Logo />
+            <AppNav />
+            <p>List of Cities</p>
+            <Footer />
+        </div>
+    );
+}
+
+export default Sidebar;
