89318dce5557fa6ef08075388c6e1cee45930754
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-19 12:57:49 -0600

    new route (app)

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import Login from "./pages/Login.jsx";
+import AppLayout from "./pages/AppLayout.jsx";
 
 export default function App() {
     return (
@@ -15,6 +16,7 @@ export default function App() {
                 <Route path="pricing" element={<Pricing/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
                 <Route path='login' element={<Login/>}/>
+                <Route path='app' element={<AppLayout/>}/>
             </Routes>
         </BrowserRouter>
     )
