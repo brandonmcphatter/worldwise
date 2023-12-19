5d6167572d00179c8835e6e9e8273631b1098616
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-19 12:17:30 -0600

    new pagenav added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,14 +1,24 @@
+import styles from "./Homepage.module.css";
 import {Link} from "react-router-dom";
+import PageNav from "../components/PageNav.jsx";
 
-function Homepage() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/product">Product</Link>
-            <br/>
-            <Link to="/pricing">Pricing</Link>
-        </div>
-    );
+export default function Homepage() {
+  return (
+    <main className={styles.homepage}>
+      <PageNav />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to='/app' className='cta'>START TRACKING NOW</Link>
+      </section>
+    </main>
+  );
 }
-
-export default Homepage;
