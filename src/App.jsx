286dbca157db85412059d5fcfc8c1f30cd0af8b3
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-19 12:16:55 -0600

    login route added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
+import Login from "./pages/Login.jsx";
 
 export default function App() {
     return (
@@ -13,6 +14,7 @@ export default function App() {
                 <Route path="product" element={<Product/>}/>
                 <Route path="pricing" element={<Pricing/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
+                <Route path='login' element={<Login/>}/>
             </Routes>
         </BrowserRouter>
     )
