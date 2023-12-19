0ecf3083d873c0840ef618c54a8230470baa787e
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-19 13:11:01 -0600

    outlet added for nested routes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,14 @@ import styles from './Sidebar.module.css';
 import Logo from "./Logo.jsx";
 import AppNav from "./AppNav.jsx";
 import Footer from "./Footer.jsx";
+import {Outlet} from "react-router-dom";
 
 function Sidebar() {
     return (
         <div className={styles.sidebar}>
             <Logo />
             <AppNav />
-            <p>List of Cities</p>
+            <Outlet />
             <Footer />
         </div>
     );
