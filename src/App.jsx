1c3e4f2c2e2742230c2147258e5407d212eee1e3
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-19 13:10:43 -0600

    new routes added

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,17 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Homepage/>}/>
+                <Route index element={<Homepage/>}/>
                 <Route path="product" element={<Product/>}/>
                 <Route path="pricing" element={<Pricing/>}/>
-                <Route path="*" element={<PageNotFound/>}/>
                 <Route path='login' element={<Login/>}/>
-                <Route path='app' element={<AppLayout/>}/>
+                <Route path='app' element={<AppLayout/>}>
+                    <Route index element={<p>All Cities</p>}/>
+                    <Route path='cities' element={<p>List of Cities</p>}/>
+                    <Route path='countries' element={<p>Countries</p>}/>
+                    <Route path='form' element={<p>Form</p>}/>
+                </Route>
+                <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
