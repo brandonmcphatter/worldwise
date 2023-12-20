6ed54d65daed4345ff65db9ff5bdf7a848ca974f
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-20 15:57:10 -0600

    specific city route added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import CityList from "./components/CityList.jsx";
 import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -41,8 +42,13 @@ export default function App() {
                         <Route index element={<CityList cities={cities}
                                                         isLoading={isLoading}
                         />}/>
+
                         <Route path='cities' element={<CityList cities={cities}
-                                                                isLoading={isLoading}/>}/>
+                                                                isLoading={isLoading}/>}
+                        />
+
+                        <Route path='cities/:id' element={<City/>}/>
+
                         <Route path='countries' element={<CountryList cities={cities}
                                                                       isLoading={isLoading}
                         />}/>
