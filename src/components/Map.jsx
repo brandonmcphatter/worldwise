0ed81267bd3726780c5a931ad3b74abbf7e75732
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-20 16:07:49 -0600

    lat / lng pass through to each component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,16 @@
 import styles from './Map.module.css';
+import {useSearchParams} from "react-router-dom";
 
 function Map() {
+    const [searchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+
     return (
         <div className={styles.mapContainer}>
-            <p>Map</p>
+            <h1>Map</h1>
+            <p>lat: {lat}</p>
+            <p>lng: {lng}</p>
         </div>
     );
 }
