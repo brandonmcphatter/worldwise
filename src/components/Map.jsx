9b400215e6c62a1dbd9e8842bfb39b4db3d686d0
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-22 21:02:58 -0600

    added leaflet map to component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,30 @@
 import styles from './Map.module.css';
 import {useNavigate, useSearchParams} from "react-router-dom";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import {useState} from "react";
 
 function Map() {
-    function changeParam() {
-        setSearchParams({lat: '23', lng: '50'})
-    }
-
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
+    console.log(lat, lng)
+    const [mapPosition] = useState([40, 0]);
 
     const navigate = useNavigate();
 
     return (
         <div className={styles.mapContainer} onClick={()=> {navigate('form')}}>
-            <h1>Map</h1>
-            <p>lat: {lat}</p>
-            <p>lng: {lng}</p>
-            <button onClick={()=> changeParam()}>
-                Change Position
-            </button>
+            <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                />
+                <Marker position={mapPosition}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     );
 }
