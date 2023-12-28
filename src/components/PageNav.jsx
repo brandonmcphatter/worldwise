2c29ea975c7a9e0b41c400578d05c16f4e39cc58
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-27 22:15:00 -0600

    login / logout functionality on pageNav

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,11 +1,14 @@
 import {NavLink} from "react-router-dom";
 import styles from './PageNav.module.css';
 import Logo from "./Logo.jsx";
+import {useAuth} from "../contexts/FakeAuthContext.jsx";
 
 function PageNav() {
+    const {isAuthenticated, logout} = useAuth();
+
     return (
         <nav className={styles.nav}>
-            <Logo />
+            <Logo/>
             <ul>
                 <li>
                     <NavLink to='/product'>Product</NavLink>
@@ -14,7 +17,10 @@ function PageNav() {
                     <NavLink to="/pricing">Pricing</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
+                    {isAuthenticated === false ?
+                        <NavLink to="/login" className={styles.ctaLink}>Login</NavLink> :
+                        <NavLink onClick={logout} to="/" className={styles.ctaLink}>Logout</NavLink>
+                    }
                 </li>
             </ul>
         </nav>
