18e4f5112ddaebf303d9d9a5cb7c81c1643de083
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-27 22:16:12 -0600

    'fake'

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,54 @@
 import styles from "./Login.module.css";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PageNav from "../components/PageNav.jsx";
 import Button from "../components/Button.jsx";
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useAuth} from "../contexts/FakeAuthContext.jsx";
 
 export default function Login() {
-  // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
-  return (
-    <main className={styles.login}>
-      <PageNav />
-      <form className={styles.form}>
-        <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
-          <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-          />
-        </div>
+    // PRE-FILL FOR DEV PURPOSES
+    const [email, setEmail] = useState("jack@example.com");
+    const [password, setPassword] = useState("qwerty");
 
-        <div className={styles.row}>
-          <label htmlFor="password">Password</label>
-          <input
-              type="password"
-              id="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-          />
-        </div>
-        <div className={styles.buttons}>
-        <Link to={'/app'}>
-          <Button type={'primary'}>Login</Button>
-        </Link>
-        </div>
-      </form>
-    </main>
-  );
+    const {login, isAuthenticated} = useAuth();
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (email && password) login(email, password);
+    }
+
+    useEffect(() => {
+        if (isAuthenticated) navigate('/app', {replace: true});
+    }, [isAuthenticated, navigate]);
+
+    return (
+        <main className={styles.login}>
+            <PageNav/>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <div className={styles.row}>
+                    <label htmlFor="email">Email address</label>
+                    <input
+                        type="email"
+                        id="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                </div>
+
+                <div className={styles.row}>
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </div>
+                <div>
+                    <Button type='primary' onClick={handleSubmit}>Login</Button>
+                </div>
+            </form>
+        </main>
+    );
 }
